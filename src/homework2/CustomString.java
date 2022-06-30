72672164d4fa1eac1108ec849cdb1cd05192d7f9
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-06-30 18:36:23 +0200

    add homework2

---
+++ src/homework2/CustomString.java
@@ -0,0 +1,188 @@
+package homework2;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.nio.charset.CharsetEncoder;
+import java.util.*;
+
+/**
+ * In this assignment, you will implement a simple class called CustomString.  This class represents a more customizable version
+ * of a String, with additional attributes and methods.
+ * For example, the CustomString class has a “reverse” method which returns a new string version of the current string where the
+ * capitalization is reversed (i.e., lowercase to uppercase and uppercase to lowercase) for the alphabetical characters specified
+ * in the given arg.  For CustomString “abc, XYZ; 123.”, calling reverse("bcdxyz@3210.") will return "aBC, xyz; 123.".
+ *
+ * The CustomString class also has a “remove” method which returns a new string version of the current string where the alphabetical
+ * characters specified in the given arg, are removed.  For CustomString "my lucky numbers are 6, 8, and 19.", calling remove("ra6")
+ * will return "my lucky numbes e 6, 8, nd 19.".
+ *
+ */
+public class CustomString {
+
+    //instance variables
+
+    /**
+     * Current string.
+     */
+    String myString;
+
+    /**
+     * Indicates if the current string myString has been set (not null).
+     */
+    boolean isSet;
+
+    //constructor
+
+    /**
+     * Initializes current string myString to null.
+     * Initializes isSet to false.
+     */
+    public CustomString() {
+        this.myString = null;
+        this.isSet = false;
+    }
+
+    // methods
+
+    /**
+     * Returns the current string.
+     * If the current string is null, or has not been set to a value with setString, it should return null.
+     * @return current string
+     */
+    public String getString() {
+        return this.myString;
+    }
+
+    /**
+     * Sets the value of the current string and sets this.isSet to true.
+     * If the given string is null, sets this.isSet to false.
+     * @param string value to be set
+     */
+    public void setString(String string) {
+        if (string == null) {
+            this.isSet = false;
+        }
+
+        this.isSet = true;
+        this.myString = string;
+    }
+
+    /**
+     * Returns a new string version of the current string where the alphabetical characters specified in the given arg, are removed.
+     *
+     * The alphabetical characters to be removed are case insensitive.
+     * All non-alphabetical characters are unaffected.
+     * If the current string is null, empty, or has not been set to a value, this method should return an empty string.
+     *
+     * Example(s):
+     * - For a current string "my lucky numbers are 6, 8, and 19.", calling remove("ra6") would return "my lucky numbes e 6, 8, nd 19."
+     * - For a current string "my lucky numbers are 6, 8, and 19.", calling remove("6,.") would return "my lucky numbers are 6, 8, and 19."
+     * - For a current string "my lucky numbers are 6, 8, and 19.", calling remove("") would return "my lucky numbers are 6, 8, and 19."
+     *
+     * Remember: This method builds and returns a new string, and does not directly modify the myString field.
+     *
+     * @param arg the string containing the alphabetical characters to be removed from the current string
+     * @return new string in which the alphabetical characters specified in the arg are removed
+     */
+    public String remove(String arg) {
+        if (this.myString != null) {
+            String regex = "(?i)["+ arg + "]";
+            String filtered = this.myString.replaceAll(regex, "");
+
+            return filtered;
+        } else {
+            return "";
+        }
+    }
+
+    /**
+     * Returns a new string version of the current string where the capitalization is reversed (i.e., lowercase to uppercase,
+     * and uppercase to lowercase) for the alphabetical characters specified in the given arg.
+     *
+     * All non-alphabetical characters are unaffected.
+     * If the current string is null, empty, or has not been set to a value, this method should return an empty string.
+     *
+     * Example(s):
+     * - For a current string "abc, XYZ; 123.", calling reverse("bcdxyz@3210.") would return "aBC, xyz; 123."
+     * - For a current string "abc, XYZ; 123.", calling reverse("6,.") would return "abc, XYZ; 123."
+     * - For a current string "abc, XYZ; 123.", calling reverse("") would return "abc, XYZ; 123."
+     * - For a current string "", calling reverse("") would return ""
+     *
+     * Remember: This method builds and returns a new string, and does not directly modify the myString field.
+     *
+     * @param arg the string containing the alphabetical characters to have their capitalization reversed in the current string
+     * @return new string in which the alphabetical characters specified in the arg are reversed
+     */
+    public String reverse(String arg) {
+        if (this.myString == null || this.myString == "") {
+            return "";
+        } else {
+            String myNewString = "";
+
+            for (char c: this.myString.toCharArray()) {
+                char currentChar = c;
+
+                for (char a: arg.toCharArray()) {
+                    if (Character.toLowerCase(a) == Character.toLowerCase(c)) {
+                        currentChar = Character.isUpperCase(c) ?
+                                Character.toLowerCase(c) : Character.toUpperCase(c);
+                    }
+                }
+
+                myNewString += currentChar;
+            }
+
+            return myNewString;
+        }
+    }
+
+    /**
+     * Returns a new string version of the current string where all the letters either >= or <= the given char n, are removed.
+     *
+     * The given letter may be a-z or A-Z, inclusive.
+     * The letters to be removed are case insensitive.
+     *
+     * If 'more' is false, all letters less than or equal to n will be removed in the returned string.
+     * If 'more' is true, all letters greater than or equal to n will be removed in the returned string.
+     *
+     * If the current string is null, the method should return an empty string.
+     * If n is not a letter (and the current string is not null), the method should return an empty string.
+     *
+     * Example(s):
+     * - For a current string "Hello 90, bye 2", calling filterLetters('h', false) would return "llo 90, y 2"
+     * - For a current string "Abcdefg", calling filterLetters('c', false) would return "defg"
+     * - For a current string "Hello 90, bye 2", calling filterLetters('h', true) would return "e 90, be 2"
+     * - For a current string "Abcdefg", calling filterLetters('c', true) would return "Ab"
+     *
+     * Remember: This method builds and returns a new string, and does not directly modify the myString field.
+     *
+     * @param n char to compare to
+     * @param more indicates whether letters <= or >= n will be removed
+     * @return new string with letters removed
+     */
+    public String filterLetters(char n, boolean more) {
+        if (this.myString == null) {
+            return "";
+        } else {
+            String myNewString = "";
+
+            for (int i = 0; i < this.myString.length(); i++) {
+                if (more) {
+                    if (Character.toLowerCase(this.myString.charAt(i)) >= n && this.myString.charAt(i) != ' ') {
+                        myNewString += "";
+                    } else {
+                        myNewString += this.myString.charAt(i);
+                    }
+                } else {
+                    if (Character.toLowerCase(this.myString.charAt(i)) <= n && this.myString.charAt(i) != ' ') {
+                        myNewString += "";
+                    } else {
+                        myNewString += this.myString.charAt(i);
+                    }
+                }
+            }
+
+            return myNewString;
+        }
+    }
+}
+
---
+++ src/homework2/CustomStringTest.java
@@ -0,0 +1,110 @@
+package homework2;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CustomStringTest {
+
+    //declare custom string for testing
+    CustomString myCustomString;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        //initialize custom string for testing
+        this.myCustomString = new CustomString();
+    }
+
+    @Test
+    void testGetString() {
+
+        //string should be null to start, before setting it
+        assertNull(this.myCustomString.getString());
+
+        this.myCustomString.setString("hello");
+        assertEquals("hello", this.myCustomString.getString());
+
+        // TODO write at least 3 additional test cases
+    }
+
+    @Test
+    void testSetString() {
+
+        //string should be null to start, before setting it
+        assertNull(this.myCustomString.getString());
+
+        this.myCustomString.setString("Good-bye!");
+        assertEquals("Good-bye!", this.myCustomString.getString());
+
+        // Set string to null;
+        this.myCustomString.setString(null);
+        assertNull(this.myCustomString.getString());
+
+        // Set string to empty string;
+        this.myCustomString.setString("");
+        assertEquals("", this.myCustomString.getString());
+
+        // Set string to my name
+        this.myCustomString.setString("Davide Di Criscito");
+        assertEquals("Davide Di Criscito", this.myCustomString.getString());
+    }
+
+    @Test
+    void testRemove() {
+        assertEquals("", this.myCustomString.remove(""));
+
+        this.myCustomString.setString(null);
+        assertEquals("", this.myCustomString.remove(""));
+
+        this.myCustomString.setString("my lucky numbers are 6, 8, and 19.");
+        assertEquals("my lucky numbes e , 8, nd 19.", this.myCustomString.remove("ra6"));
+
+        this.myCustomString.setString("My name is Davide Di Criscito");
+        assertEquals("My name s ave  Crscto", this.myCustomString.remove("Di"));
+
+        this.myCustomString.setString("TesTing CaSe InSesItive");
+        assertEquals("eing Cae IneIive", this.myCustomString.remove("ts"));
+
+        this.myCustomString.setString("ThIs MeThOd Is CaSe InSeSiTiVe");
+        assertEquals("Ths eThOd s CSe nSeSTVe", this.myCustomString.remove("mai"));
+    }
+
+    @Test
+    void testReverse() {
+        assertEquals("", this.myCustomString.reverse(""));
+
+        this.myCustomString.setString(null);
+        assertEquals("", this.myCustomString.reverse(""));
+
+        this.myCustomString.setString("abc, XYZ; 123.");
+        assertEquals("aBC, xyz; 123.", this.myCustomString.reverse("bcdxyz@3210."));
+
+        this.myCustomString.setString("TeStInG CaSe");
+        assertEquals("tEsTInG CasE", this.myCustomString.reverse("test"));
+
+        this.myCustomString.setString("asdf123  double Space");
+        assertEquals("ASdf123  doublE spAcE", this.myCustomString.reverse("123sae"));
+
+        this.myCustomString.setString("LOWER case upper CASE");
+        assertEquals("lower casE uppER CASe", this.myCustomString.reverse("LOWER"));
+    }
+
+    @Test
+    void testFilterLetters() {
+        assertEquals("", this.myCustomString.filterLetters('E', false));
+
+        this.myCustomString.setString(null);
+        assertEquals("", this.myCustomString.filterLetters('E', false));
+
+        this.myCustomString.setString("abcdefghz");
+        assertEquals("abcdef", this.myCustomString.filterLetters('g', true));
+
+        this.myCustomString.setString("My name is Davide Di Criscito");
+        assertEquals("My nm is vi i risito", this.myCustomString.filterLetters('f', false));
+
+        this.myCustomString.setString("Testing space");
+        assertEquals("Tstin sp", this.myCustomString.filterLetters('h', false));
+    }
+
+}
+
